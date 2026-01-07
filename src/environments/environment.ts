1c47443996f22f37029d9b7e7d706de6999cfbbd
Morteza Malvandi <malvandi.m@gmail.com>
2026-01-07 19:03:43 +0330

    Refactor activity report retrieval logic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   // ktapi: 'http://localhost:9030',
   ktapi: 'https://trader.bamap.ir:444/kt-api',
-  napi: 'https://trader.bamap.ir:444/api'
+  napi: 'http://localhost:9002'
+  // napi: 'https://trader.bamap.ir:444/api'
 };
